179ec252c1cfba0c87825b541ef37f3b7cc68ad5
Mandula Naresh Babu <naresh2223@gmail.com>
2019-12-04 23:01:07 -0500

    Checked-In the Project
    Java Project check-in to Github

---
+++ src/basics/DataTypes.java
@@ -0,0 +1,78 @@
+package basics;
+
+public class DataTypes {
+
+	// Characteristics or attributes or data members
+	// Primitive data types
+	int a=10;
+	float b = 12.4f;
+	boolean c = true;
+	boolean d = false;
+	double e = 234.345434;
+	char f = 'g';
+	byte h = 127;
+	long i = 3432425;
+	short s = 2345;
+	
+	// Behavior
+	public void Addition()
+	{
+		int c1 = a+20;
+		System.out.println("Addition ouput is : " + c1);
+	}
+	
+	public void Subtraction()
+	{
+		int c1 = a-20;
+		System.out.println("Subtraction ouput is : " + c1);
+	}
+	
+	public void Mutiplication()
+	{
+		double c = a*b;
+		System.out.println("Multiplication value of a and b is : " + c);
+	}
+	
+	public void Division()
+	{
+		double c = 20/a;
+		System.out.println("Division value of a and b is : " + c);
+	}
+	
+	
+	public void Modulus()
+	{
+		double c = 20%a;
+		System.out.println("Modulus value of a and b is : " + c);
+	}
+	
+	// Entry point of Java
+	// Method name - main
+	// Main method will be called by the JVM (Java Virtual Machine)
+	// Main method is static hence JVM need not to create an object to call this main method
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world..! welcome to the DataTypes training...!");
+	
+		// Classname objName = new Constructor name (Always Constructor name will be same as a class name)
+		DataTypes dt = new DataTypes();
+		
+		System.out.println(dt.a);
+		System.out.println(dt.b);
+		System.out.println(dt.c);
+		System.out.println(dt.d);
+		System.out.println(dt.e);
+		System.out.println(dt.f);
+		System.out.println(dt.h);
+		System.out.println(dt.i);
+		System.out.println(dt.s);
+		
+		dt.Addition();
+		dt.Subtraction();
+		dt.Mutiplication();
+		dt.Division();
+		dt.Modulus();
+			
+	}
+
+}
---
+++ src/practice/IfExample.java
@@ -0,0 +1,23 @@
+package practice;
+
+//Java Program to demonstrate the use of if statement.  
+public class IfExample {  
+	
+	public static void main(String[] args) {  
+		//defining an 'age' variable  
+		int age=18;  
+		//checking the age  
+		if(age>18){  
+			System.out.print("Age is greater than 18");  
+		}
+		else if(age==18)
+		{
+			System.out.println("Age is equal to 18");
+		}
+		else
+		{
+			System.out.println("Age is less than 18");
+		}
+	}  
+	
+}  
